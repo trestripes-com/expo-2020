05404721d0c112383633ce3e29fb3dd957252a76
Martin Toman <toman.martin@live.com>
2020-10-07 12:58:23 +0200

    Make cursor component only-mobile

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ require("aframe-layout-component")
 require("aframe-template-component")
 require("aframe-event-set-component")
 require("aframe-look-at-component")
+
+AFRAME.registerComponent('only-mobile', {
+  init: function () {
+    if (!AFRAME.utils.device.isMobile())
+      this.el.parentNode.removeChild(this.el);
+  }
+})
