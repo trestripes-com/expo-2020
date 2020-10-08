e2479d8915b2cc028d2745c4116c9061985a0d9e
Martin Toman <toman.martin@live.com>
2020-10-08 21:02:05 +0200

    Remove hover-cursor

--- src/index.js
+++ src/index.js
@@ -2,10 +2,3 @@ require("aframe")
 require("aframe-layout-component")
 require("aframe-template-component")
 require("aframe-look-at-component")
-
-AFRAME.registerComponent('only-mobile', {
-  init: function () {
-    if (!AFRAME.utils.device.isMobile())
-      this.el.parentNode.removeChild(this.el);
-  }
-})
